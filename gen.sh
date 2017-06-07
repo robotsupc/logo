dccbc6d37a1d2fecf9b8ee89903cbb84ed28757e
Luuk W. (Twinone) <twinonedevs@gmail.com>
2017-06-07 16:45:02 +0200

    Add webp

--- gen.sh
+++ gen.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-rm -r jpg
+rm -rf jpg
 cp -r png jpg
-cd jpg
+pushd jpg
 mogrify -background white -alpha remove -format jpg *.png
 rm *.png
+popd
+
+QUALITIES="60 80 100"
+for qual in $QUALITIES; do
+	rm -rf webp-$qual
+	cp -r png webp-$qual
+	pushd webp-$qual
+	for i in $(ls); do
+		filename=$(basename "$i")
+		extension="${filename##*.}"
+		filename="${filename%.*}"
+		cwebp -q $qual $filename.$extension -o $filename.webp
+	done
+	rm *.png
+	popd
+
+done
